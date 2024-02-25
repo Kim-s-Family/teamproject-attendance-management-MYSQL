8f2643b74b3dd45c292c4ec5ee3be4af3e73b67e
LEEMINKUK <158013587+LEEMINKUK@users.noreply.github.com>
2024-02-25 17:20:23 +0900

    수정3(2024.02.25)
    수정3(2024.02.25)

--- insert_evaluation_list_table.sql
+++ insert_evaluation_list_table.sql
@@ -1,40 +1,43 @@
 INSERT INTO
-		   evaluation_list(penalty_factor,  -- 감점요인  == varchar
-                           penalty_score,   -- 페널티 점수  == integer
-                           evaluation_code, -- 평가코드  == varchar
-                           attendance_code, -- 근태코드  == varchar
-                           emp_id)          -- 사용자ID  == varchar
+		   evaluation_list(
+						   evaluation_code, -- 평가코드  == varchar
+                           emp_id,		    -- 사용자ID  == varchar
+						   attendance_code, -- 근태코드  == varchar
+                           penalty_factor,  -- 감점요인  == varchar
+                           penalty_score   -- 페널티 점수  == integer
+                           )          
 		VALUES
-    (NULL,NULL,'eva1','att1','user001'),
-    ('지각',-1,'eva2','att2','user002'),
-    (NULL,NULL,'eva3','att3','user003'),
-    ('결근',-3,'eva4','att4','user004'),
-    (NULL,NULL,'eva5','att5','user005'),
-    ('지각',-1,'eva6','att6','user006'),
-    (NULL,NULL,'eva7','att7','user007'),
-    ('결근',-3,'eva8','att8','user008'),
-    (NULL,NULL,'eva9','att9','user009'),
-    ('지각',-1,'eva10','att10','user010'),
-    (NULL,NULL,'eva11','att11','user011'),
-    ('결근',-3,'eva12','att12','user012'),
-    (NULL,NULL,'eva13','att13','user013'),
-    ('지각',-1,'eva14','att14','user014'),
-    (NULL,NULL,'eva15','att15','user015'),
-    ('결근',-3,'eva16','att16','user016'),
-    (NULL,NULL,'eva17','att17','user017'),
-    ('지각',-1,'eva18','att18','user018'),
-    (NULL,NULL,'eva19','att19','user019'),
-    ('결근',-3,'eva20','att20','user020'),
-    (NULL,NULL,'eva21','att21','user021'),
-    ('지각',-1,'eva22','att22','user022'),
-    (NULL,NULL,'eva23','att23','user023'),
-    ('결근',-3,'eva24','att24','user024'),
-    (NULL,NULL,'eva25','att25','user025'),
-    ('지각',-1,'eva26','att26','user026'),
-    (NULL,NULL,'eva27','att27','user027'),
-    ('결근',-3,'eva28','att28','user028'),
-    (NULL,NULL,'eva29','att29','user029'),
-    ('지각',-1,'eva30','att30','user030');
- -- (감점요인,페널티점수,평가코드,근태코드,사용자ID)
+    ('eva001', 'user002', 'att32', '지각', -1),
+    ('eva002', 'user004', 'att34', '결근', -3),
+    ('eva003', 'user006', 'att36', '지각', -1),
+    ('eva004', 'user020', 'att50', '지각', -1),
+    ('eva005', 'user022', 'att52', '지각', -1),
+    ('eva006', 'user030', 'att60', '지각', -1),
+    ('eva007', 'user004', 'att124', '지각', -1),
+    ('eva008', 'user028', 'att148', '지각', -1),
+    ('eva009', 'user029', 'att149', '결근', -3),
+    ('eva010', 'user011', 'att161', '결근', -3),
+    ('eva011', 'user005', 'att185', '지각', -1),
+    ('eva012', 'user012', 'att192', '지각', -1),
+    ('eva013', 'user003', 'att303', '지각', -1),
+    ('eva014', 'user023', 'att323', '지각', -1),
+    ('eva015', 'user007', 'att337', '결근', -3),
+    ('eva016', 'user023', 'att383', '결근', -3),
+    ('eva017', 'user011', 'att401', '지각', -1),
+    ('eva018', 'user028', 'att418', '지각', -1),
+    ('eva019', 'user010', 'att430', '지각', -1),
+    ('eva020', 'user029', 'att449', '지각', -1),
+    ('eva021', 'user011', 'att461', '결근', -3),
+    ('eva022', 'user017', 'att557', '결근', -3),
+    ('eva023', 'user014', 'att584', '지각', -1),
+    ('eva024', 'user026', 'att596', '지각', -1),
+    ('eva025', 'user003', 'att633', '지각', -1),
+    ('eva026', 'user004', 'att664', '지각', -1),
+    ('eva027', 'user020', 'att680', '지각', -1),
+    ('eva028', 'user029', 'att689', '지각', -1);
+    
+
+ -- (평가코드, 직원ID, 평가코드, 감점요인, 페널티 점수)
 
 SELECT * FROM evaluation_list;
+-- 		WHERE emp_id = 'user003'; 
